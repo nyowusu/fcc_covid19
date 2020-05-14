747f1fb04da5482c592e4030506a15159331101b
nyowusu <yaw.owusu@hotmail.com>
2020-05-14 11:55:55 +0100

    pages and components setup

---
+++ src/components/table-container.svelte
@@ -0,0 +1,9 @@
+<script>
+  import TableFilter from "./table-filter.svelte";
+  import Table from "./table.svelte";
+</script>
+
+<h1>Table Container</h1>
+
+<TableFilter />
+<Table />
--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,10 +1,18 @@
+<script context="module">
+  export async function preload(page) {
+    return { state: page.params['state'] };
+  }
+</script>
+
 <script>
   import CovidState from "../components/covid-stat.svelte";
   import CovidChart from "../components/covid-chart.svelte";
   import TableContainer from "../components/table-container.svelte";
+
+  export let state;
 </script>
 
-<h1>Index</h1>
+<h1>hello: {state}</h1>
+
 <CovidState />
 <CovidChart />
-<TableContainer /> 
